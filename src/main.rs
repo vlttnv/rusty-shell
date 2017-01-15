2cf861c0160a4214363af73aa47c1049e445b224
Valentin Tunev <hdbuster@gmail.com>
2017-01-15 12:44:14 +0200

    Some error handling and 2 unit tests

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ use std::io::stdout;
 use std::process::Command;
 use std::env;
 
+const BUILT_IN_COMMANDS: &'static [ &'static str ] = &["cd"];
+
 fn main() {
     // Load config files.
 
     // Run command loop
-    let exit_code = rusty_shell_loop();
+    // let exit_code = rusty_shell_loop();
+    rusty_shell_loop();
 
     // Perform shutdown/cleanup
 
+    // Not handling this yet
     // Exit
-    std::process::exit(exit_code);
+    // std::process::exit(exit_code);
 }
 
 
@@ -35,12 +39,14 @@ fn rusty_shell_loop() -> i32 {
         args = rsh_parse_line(&line);
         exit_code = rsh_run(&args);
 
-        if exit_code != 0 {
-            break;
-        }
+        // TODO: Handle exit
+        // if exit_code != 0 {
+        //     break;
+        // }
     }
 
-    exit_code
+    // Not handling this yet
+    // exit_code
 }
 
 fn show_prompt() {
@@ -51,20 +57,35 @@ fn show_prompt() {
 
 fn rsh_cd(args: &Vec<&str>) {
     if args.len() == 1 {
-        print!("rsh: expected arguments to \"cd\"");
+        println!("rsh: expected arguments to \"cd\"");
+    } else {
+        match env::set_current_dir(args[1]) {
+            Ok(result) => result,
+            Err(err) => { println!("{}", err);}
+        }
     }
 
 }
 
 /// Split a line into command and args
-fn rsh_parse_line(line: &str) -> Vec<&str> {
+pub fn rsh_parse_line(line: &str) -> Vec<&str> {
     let args: Vec<&str> = line.split_whitespace().collect();
     args
 }
 
-fn rsh_run(args: &Vec<&str>) -> i32 {
+pub fn rsh_run(args: &Vec<&str>) -> i32 {
     if args.len() == 0 {
         return 0;
+    }
+    
+    // Draft way of handling built in commands
+    if BUILT_IN_COMMANDS.contains(&args[0]) {
+        if args[0] == "cd" {
+            rsh_cd(&args);
+        }
+
+        // TODO: propagate code
+        return 0
     }
 
     let mut command = Command::new(args[0]);
@@ -73,19 +94,33 @@ fn rsh_run(args: &Vec<&str>) -> i32 {
         command.args(&args[1..args.len()]);
     }
 
-
-    // let status = command.status().unwrap_or_else(|e| { println!("{:?}", e); None });
-    
-    // Pattern match to retrieve the value
-    // match status.code() {
-    //     Some(x) => x,
-    //     None    => -1,
-    // }
+    // Try to get the code and return it
+    // Print the error otherwise
     match command.status() {
-        Ok(result) => match result.code() {
-            Some(code)  => code,
-            None        => -1,
-        },
-        Err(err) => { println!("{:?}", err); 0 }
+        Ok(result) => result.code().unwrap_or(-1),
+        Err(err) => { println!("{}: {}", args[0], err); 0 }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rsh_parse_line() {
+        let command = "ls -l -a";
+        let args: Vec<&str>;
+        args = rsh_parse_line(&command);
+        assert_eq!(args.len(), 3);
+        assert_eq!(args[0], "ls");
+        assert_eq!(args[1], "-l");
+        assert_eq!(args[2], "-a");
+    }
+
+    #[test]
+    fn test_rsh_run() {
+        let parsed_command = vec!["pwd"];
+        let exit_code = rsh_run(&parsed_command);
+        assert_eq!(exit_code, 0);
     }
 }
